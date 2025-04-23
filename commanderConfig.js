bfc199774d63ce55e157dc2c30b09c1eaa0307ee
Corentin-CESI <150168453+Corentin-CESI@users.noreply.github.com>
2025-04-23 10:11:43 +0200

    Extract command line argument conditions to function

--- commanderConfig.js
+++ commanderConfig.js
@@ -1,23 +1,22 @@
 var commander = require('commander');
 var image = require('./Image');
-// The version should be the same as the package.json version, keep it in sync
-module.exports = commander.version('1.11.0','-v, --version')
-  .option('-n, --number [integer]', 'Number of files to generate',function(numberFileToGenerateInput){
-    'use strict';
-    var numberFileToGenerate = parseInt(numberFileToGenerateInput);
-    // n should be a valid number
-    if(isNaN(numberFileToGenerate)){return 1}
-    return numberFileToGenerate;
-  },1)
-  .option('-s, --size [1024x768]', 'Image size',function(imageSize){
-    'use strict';
-    // Check for an "x" in the string
-    // and if not, return the default size
-    // 1024x768
-    if (imageSize.indexOf('x')===-1){return '1024x768'}
-    return imageSize;
-  },'1024x768')
-  .option('-p, --provider [provider]', 'Set the image provider; '+Object.keys(image.providers).join(', '),
+
+function parseNumber(n){
+  'use strict';
+  var n2 = parseInt(n,10);
+  return isNaN(n2) ? 1 : n2;
+}
+
+function parseSize(size) {
+  'use strict';
+  return size.indexOf('x') === -1 ? '1024x768' : size;
+}
+
+module.exports = commander
+  .version('1.11.0','-v, --version')
+  .option('-n, --number [integer]', 'Number of files to generate', parseNumber, 1)
+  .option('-s, --size [1024x768]', 'Image size',parseSize, '1024x768')
+  .option('-p, --provider [provider]', 'Set the image provider; '+Object.keys(image.list).join(', '),
     function(provider){
       'use strict';
       // Set image provider
