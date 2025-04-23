b2d9ab8a4dc68de1dd9eed9ad5faebc3a076eea9
Raspy <enora.broudic@clever-cloud.com>
2025-04-23 09:41:11 +0200

    Refactor Image class: Move 'use strict' to file level and simplify constructor

--- Image.js
+++ Image.js
@@ -1,28 +1,23 @@
-class Image {
-  size;
-  provider;
-  providers;
+'use strict';
 
-  constructor(size, provider) {
-    this.size = size;
+class Image {
+  constructor(provider) {
     this.provider = provider;
+
     this.providers = {
       DummyImage: {
         getImageUrl: function (size) {
-          'use strict';
           return 'https://dummyimage.com/'+size+'/000/fff';
         }
       },
       LoremPicsum: {
         getImageUrl: function (size) {
-          'use strict';
           var [width,height]=size.split('x');
           return 'https://picsum.photos/'+width+'/'+height+'/?random';
         }
       },
       FakeImg: {
         getImageUrl: function (size) {
-          'use strict';
           var [width,height]=size.split('x');
           return 'https://fakeimg.pl/'+width+'x'+height+'/384f66/ecf0f1/?text=Spaceholder&font=lobster';
         }
@@ -31,30 +26,32 @@ class Image {
   }
 
   getImageUrl(size) {
-    'use strict';
     return this.providers[this.getProvider()].getImageUrl(size);
   }
 
   setProvider(provider) {
-    'use strict';
     this.provider = provider;
   }
   
   getProvider() {
-    'use strict';
-    if (this.provider==='random') {
-      var l=Object.keys(this.providers);
-      var i=Math.floor(Math.random()*l.length);
-      return l[i];
+    if (this.provider === 'random') {
+      return this.selectRandomProvider();
     }
+
     return this.provider;
+  }
+
+  selectRandomProvider() {
+    var providersKeys = Object.keys(this.providers);
+    var randomIndex = Math.floor(Math.random() * providersKeys.length);
+    
+    return providersKeys[randomIndex];
   }
 }
 
-
 module.exports = {
   Image: Image,
-  createImage: function(size, provider = 'random') {
-    return new Image(size, provider);
+  createImage: function(provider = 'random') {
+    return new Image(provider);
   }
 }
