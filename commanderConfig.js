90aac9bfe7f15eb4456c18934d08bfaa34dce791
FredVinet <fvinet9@gmail.com>
2025-04-23 09:13:30 +0200

    Rajout de constantes pour les valeurs par défaut

--- commanderConfig.js
+++ commanderConfig.js
@@ -1,22 +1,26 @@
 var commander = require('commander');
 var image = require('./Image');
+
+const DEFAULT_NUMBER = 1;
+const DEFAULT_SIZE = '1024x768';
+
 // The version should be the same as the package.json version, keep it in sync
 module.exports = commander.version('1.11.0','-v, --version')
   .option('-n, --number [integer]', 'Number of files to generate',function(numberFileToGenerateInput){
     'use strict';
     var numberFileToGenerate = parseInt(numberFileToGenerateInput);
     // n should be a valid number
-    if(isNaN(numberFileToGenerate)){return 1}
+    if(isNaN(numberFileToGenerate)){return DEFAULT_NUMBER}
     return numberFileToGenerate;
-  },1)
+  }, DEFAULT_NUMBER)
   .option('-s, --size [1024x768]', 'Image size',function(imageSize){
     'use strict';
     // Check for an "x" in the string
     // and if not, return the default size
     // 1024x768
-    if (imageSize.indexOf('x')===-1){return '1024x768'}
+    if (imageSize.indexOf('x')===-1){return DEFAULT_SIZE}
     return imageSize;
-  },'1024x768')
+  }, DEFAULT_SIZE)
   .option('-p, --provider [provider]', 'Set the image provider; '+Object.keys(image.providers).join(', '),
     function(provider){
       'use strict';
