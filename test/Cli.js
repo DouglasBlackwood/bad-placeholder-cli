1a00f188e85f635053b550adf64ba230bbef65c6
Charly Laurent <douglas.blackwood@protonmail.com>
2026-03-18 06:33:17 +0100

    Fix issues before workshop (#39)
    - Fix test command
    - Remove unstable test
    
    ---------
    
    Co-authored-by: Charly Laurent <charly.laurent@protonmail.com>

--- test/Cli.js
+++ test/Cli.js
@@ -3,113 +3,69 @@ var expect = require('chai').expect;
 var execa = require('execa');
 var helpers = require('./helpers');
 
-describe('Symlink', function () {
+describe('bad-placeholder -n 3', function() {
   'use strict';
 
-  before(function (done) {
+  before(function(done) {
     helpers.deletePlaceholders()
-      .then(function (response) {
+      .then(function(response) {
         done();
       });
   });
 
   var result;
 
-  before(function (done) {
-    execa.shell('npm link')
-      .then(function (response) {
-        result = response.stdout;
-        done();
-      })
-      .catch(function (error) {
-        console.log(error);
-        done();
-      });
-  });
-
-  var result2;
-
-  before(function (done) {
-    execa.shell('bad-placeholder')
-      .then(function (response) {
-        result2 = response.stdout;
-        done();
-      })
-      .catch(function (error) {
-        console.log(error);
-        done();
-      });
-  });
-
-  it('should generate one image with default dimensions', function () {
-    var dimensions = helpers.getDimensions(helpers.getPlaceholders()[0]);
-    expect(dimensions).to.be.equal('1024x768');
-  });
-});
-
-describe('bad-placeholder -n 3', function () {
-  'use strict';
-
-  before(function (done) {
-    helpers.deletePlaceholders()
-      .then(function (response) {
-        done();
-      });
-  });
-
-  var result;
-
-  before(function (done) {
+  before(function(done) {
     execa.shell('node ./index.js -n 3')
-      .then(function (response) {
+      .then(function(response) {
         result = response.stdout;
         done();
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
         done();
       });
   });
 
-  it('should generate three images', function () {
+  it('should generate three images', function() {
     expect(helpers.getPlaceholders().length).to.be.equal(3);
   });
 
-  it('should have default dimensions', function () {
+  it('should have default dimensions', function() {
     var dimensions = helpers.getDimensions(helpers.getPlaceholders()[Math.floor(Math.random() * 2) + 0]);
     expect(dimensions).to.be.equal('1024x768');
   });
 });
 
-describe('bad-placeholder -s 100x100', function () {
+describe('bad-placeholder -s 100x100', function() {
   'use strict';
 
-  before(function (done) {
+  before(function(done) {
     helpers.deletePlaceholders()
-      .then(function (response) {
+      .then(function(response) {
         done();
       });
   });
 
   var result;
 
-  before(function (done) {
+  before(function(done) {
     execa.shell('node ./index.js -s 100x100')
-      .then(function (response) {
+      .then(function(response) {
         result = response.stdout;
         done();
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
         done();
       });
   });
 
-  it('should generate one image', function () {
+  it('should generate one image', function() {
     expect(helpers.getPlaceholders().length).to.be.equal(1);
   });
 
-  it('should be 100x100', function () {
+  it('should be 100x100', function() {
     var dimensions = helpers.getDimensions(helpers.getPlaceholders()[0]);
     expect(dimensions).to.be.equal('100x100');
   });
@@ -118,42 +74,42 @@ describe('bad-placeholder -s 100x100', function () {
 for (const provider of ["DummyImage", "LoremPicsum", "FakeImg"]) {
   'use strict';
 
-  describe('bad-placeholder -p ' + provider, function () {
+  describe('bad-placeholder -p ' + provider, function() {
     'use strict';
 
-    before(function (done) {
+    before(function(done) {
       helpers.deletePlaceholders()
-        .then(function (response) {
+        .then(function(response) {
           done();
         });
     });
 
     var result;
 
-    before(function (done) {
+    before(function(done) {
       execa.shell('node ./index.js -p ' + provider)
-        .then(function (response) {
+        .then(function(response) {
           result = response.stdout;
           done();
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
           done();
         });
     });
 
-    it('should generate one image from specified provider (' + provider + ')', function () {
+    it('should generate one image from specified provider (' + provider + ')', function() {
       expect(helpers.getPlaceholders().length).to.be.equal(1);
     });
 
-    it('should have default dimensions', function () {
+    it('should have default dimensions', function() {
       var dimensions = helpers.getDimensions(helpers.getPlaceholders()[0]);
       expect(dimensions).to.be.equal('1024x768');
     });
 
-    after(function (done) {
+    after(function(done) {
       helpers.deletePlaceholders()
-        .then(function (response) {
+        .then(function(response) {
           done();
         });
     });
