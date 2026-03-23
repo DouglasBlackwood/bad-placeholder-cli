22326bb818e5d88f58036ccfe9e41c79db49ed88
Marie Eustace <marie.eustace@viacesi.fr>
2026-03-23 16:11:46 +0100

    refactor: rename generateRandomFileName + fileNumber

--- index.js
+++ index.js
@@ -49,15 +49,15 @@ var downloadPlaceHolder = (a, b) => {
 };
 fr.maxRedirects = 10;
 // Generate a randome file name
-var generateFileName = (idFile) => (
+var generateRandomFileName = (fileNumber) => (
 		"placeholder_" +
 		cmd.size +
 		"_" +
 		rdm({ length: 4 }) +
-		idFile +
+		fileNumber +
 		rdm({ length: 4 }) +
 		".jpg"
 	);
 for (i = 1; i <= cmd.number; i++) {
-	downloadPlaceHolder(img.getImgUrl(cmd.size), generateFileName(i));
+	downloadPlaceHolder(img.getImgUrl(cmd.size), generateRandomFileName(i));
 }
