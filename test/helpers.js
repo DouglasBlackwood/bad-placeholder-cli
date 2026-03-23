ae3a030a98a99f13e91567e47bf3a89356b2d55e
JohanBZH <m.johan.mons@gmail.com>
2026-03-23 11:56:28 +0100

    Format

--- test/helpers.js
+++ test/helpers.js
@@ -1,41 +1,41 @@
-var fs = require('fs');
+var fs = require("fs");
 
 module.exports = {
-  getRootFiles: function () {
-    return fs.readdirSync('./');
-  },
+	getRootFiles: function () {
+		return fs.readdirSync("./");
+	},
 
-  getPlaceholders: function () {
-    var files = this.getRootFiles();
+	getPlaceholders: function () {
+		var files = this.getRootFiles();
 
-    return files.filter(function (filename) {
-      if (filename.indexOf('placeholder_') !== -1) {
-        return filename
-      }
-    });
-  },
+		return files.filter(function (filename) {
+			if (filename.indexOf("placeholder_") !== -1) {
+				return filename;
+			}
+		});
+	},
 
-  deletePlaceholders: function () {
-    var placeholders = this.getPlaceholders();
-    var count = placeholders.length;
+	deletePlaceholders: function () {
+		var placeholders = this.getPlaceholders();
+		var count = placeholders.length;
 
-    return new Promise(function (resolve, reject) {
-      if (count) {
-        placeholders.forEach(function (filename) {
-          fs.unlinkSync(filename);
-          count--;
+		return new Promise(function (resolve, reject) {
+			if (count) {
+				placeholders.forEach(function (filename) {
+					fs.unlinkSync(filename);
+					count--;
 
-          if (!count) {
-            resolve();
-          }
-        });
-      }
+					if (!count) {
+						resolve();
+					}
+				});
+			}
 
-      resolve();
-    });
-  },
+			resolve();
+		});
+	},
 
-  getDimensions: function (filename) {
-    return filename.split('_')[1];
-  }
+	getDimensions: function (filename) {
+		return filename.split("_")[1];
+	},
 };
