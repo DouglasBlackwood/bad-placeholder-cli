827d27fbd625671f831ac46834a9a1dfaaa17c80
Yoann Meynsan <yoann.meynsan@viacesi.fr>
2026-03-24 10:10:03 +0100

    refactor: rename variables and functions in image.js

--- Image.js
+++ Image.js
@@ -1,43 +1,42 @@
 module.exports = {
-	list: {
-		// DummyImage
-		DummyImage: {
-			getImgUrl: (s) => "https://dummyimage.com/" + s + "/000/fff",
-		},
-		// LoremPicsum
-		LoremPicsum: {
-			getImgUrl: (s) => {
-				
-				var s2 = s.split("x");
-				return "https://picsum.photos/" + s2[0] + "/" + s2[1] + "/?random";
-			},
-		},
-		// PlaceBear
-		PlaceBear: {
-			getImgUrl: (s) => {
-				
-				var s2 = s.split("x");
-				return "https://placebear.com/" + s2[0] + "/" + s2[1];
-			},
-		},
-	},
-	currentProvider: "random",
-	getPrvd: function () {
-		
-		// Select random image provider
-		if (this.currentProvider === "random") {
-			var providers = Object.keys(this.list);
-			var randomIndex = Math.floor(Math.random() * providers.length);
-			return providers[randomIndex];
-		}
-		return this.currentProvider;
-	},
-	setPrvd: function (currentProvider) {
-		
-		this.currentProvider = currentProvider;
-	},
-	getImgUrl: function (size) {
-		
-		return this.list[this.getPrvd()].getImgUrl(size);
-	},
+    list: {
+        // DummyImage
+        DummyImage: {
+            getImageUrl: (imageSize) => "https://dummyimage.com/" + imageSize + "/000/fff",
+        },
+        // LoremPicsum
+        LoremPicsum: {
+            getImageUrl: (imageSize) => {
+                var tabImageSize = imageSize.split("x");
+                return "https://picsum.photos/" + tabImageSize[0] + "/" + tabImageSize[1] + "/?random";
+            },
+        },
+        // PlaceBear
+        PlaceBear: {
+            getImageUrl: (imageSize) => {
+
+                var tabImageSize = imageSize.split("x");
+                return "https://placebear.com/" + tabImageSize[0] + "/" + tabImageSize[1];
+            },
+        },
+    },
+    currentProvider: "random",
+    getProvider: function () {
+
+        // Select random image provider
+        if (this.currentProvider === "random") {
+            var tabProvider = Object.keys(this.list);
+            var randomNumber = Math.floor(Math.random() * tabProvider.length);
+            return tabProvider[randomNumber];
+        }
+        return this.currentProvider;
+    },
+    setProvider: function (currentProvider) {
+
+        this.currentProvider = currentProvider;
+    },
+    getImageUrl: function (imageSize) {
+
+        return this.list[this.getProvider()].getImageUrl(imageSize);
+    },
 };
