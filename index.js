169ccd8879d491af38aba3fa60119c01a1c0bdeb
Lilian Fischer <lilian.fischer@viacesi.fr>
2026-03-24 10:41:42 +0100

    refactor: whole index.js file

--- index.js
+++ index.js
@@ -8,56 +8,102 @@ var image = require("./Image");
 var readline = require("readline");
 /* Require Commander configuration */
 var cliOptions = require("./commanderConfig");
+
 // Counter of files downloaded
 var downloadedFileCounter = 0;
 // List of files downloaded
 var downloadedFiles = [];
-// Download an image
+
+function pipeResponseToFile(response, fileStream) {
+	response.pipe(fileStream);
+}
+
+function updateDownloadState(imageFileName) {
+	downloadedFileCounter++;
+	downloadedFiles.push(imageFileName);
+}
+
+function renderProgress() {
+	var downloadProgress = Math.ceil(
+		(downloadedFileCounter / cliOptions.number) * 100
+	);
+
+	readline.cursorTo(process.stdout, 0);
+	process.stdout.write(
+		"Downloaded " +
+			downloadedFileCounter +
+			" of " +
+			cliOptions.number +
+			". [" +
+			downloadProgress +
+			" %]"
+	);
+}
+
+function handleDownloadCompletion(imageFileName) {
+	updateDownloadState(imageFileName);
+	renderProgress();
+
+	if (downloadedFileCounter === cliOptions.number) {
+		console.info(
+			"\n" + cliOptions.number + " image(s) successfully downloaded"
+		);
+	}
+}
+
+function onFileStreamFinish(fileStream, imageFileName) {
+	fileStream.close(function onClose() {
+		handleDownloadCompletion(imageFileName);
+	});
+}
+
+function onFileStreamError() {
+	console.log("Failed");
+}
+
+function attachFileStreamEvents(fileStream, imageFileName) {
+	fileStream.on("finish", function handleFinish() {
+		onFileStreamFinish(fileStream, imageFileName);
+	});
+
+	fileStream.on("error", function handleError() {
+		onFileStreamError();
+	});
+}
+
+function handleResponse(response, fileStream, imageFileName) {
+	pipeResponseToFile(response, fileStream);
+	attachFileStreamEvents(fileStream, imageFileName);
+}
+
 var downloadPlaceHolder = (imageUrl, imageFileName) => {
-	
 	var fileStream = fileSystem.createWriteStream(imageFileName);
-	var handle = (response) => {
-		response.pipe(fileStream);
-		fileStream.on("finish", () => {
-			fileStream.close(() => {
-				
-				downloadedFileCounter++;
-				downloadedFiles.push(imageFileName);
-				var downloadProgress = Math.ceil((downloadedFileCounter / cliOptions.number) * 100);
-				readline.cursorTo(process.stdout, 0);
-				process.stdout.write(
-					"Downloaded " + downloadedFileCounter + " of " + cliOptions.number + ". [" + downloadProgress + " %]",
-				);
-				if (downloadedFileCounter === cliOptions.number) {
-					console.info("\n" + cliOptions.number + " image(s) successfully downloaded");
-				}
-			});
-		});
-		fileStream.on("error", () => {
-			console.log("Failed");
-		});
-	};
+
 	if (imageUrl.substring(0, 7) === "http://") {
-		http.get(imageUrl, (response) => {
-			handle(response);
+		http.get(imageUrl, function onHttpResponse(response) {
+			handleResponse(response, fileStream, imageFileName);
 		});
 	} else {
-		https.get(imageUrl, (response) => {
-			handle(response);
+		https.get(imageUrl, function onHttpsResponse(response) {
+			handleResponse(response, fileStream, imageFileName);
 		});
 	}
 };
+
 followRedirects.maxRedirects = 10;
-// Generate a randome file name
-var generateRandomFileName = (fileNumber) => (
-		"placeholder_" +
-		cliOptions.size +
-		"_" +
-		randomString({ length: 4 }) +
-		fileNumber +
-		randomString({ length: 4 }) +
-		".jpg"
-	);
+
+var generateRandomFileName = (fileNumber) =>
+	"placeholder_" +
+	cliOptions.size +
+	"_" +
+	randomString({ length: 4 }) +
+	fileNumber +
+	randomString({ length: 4 }) +
+	".jpg";
+
 for (i = 1; i <= cliOptions.number; i++) {
-	downloadPlaceHolder(image.getImgUrl(cliOptions.size), generateRandomFileName(i));
+	downloadPlaceHolder(
+		image.getImgUrl(cliOptions.size),
+		generateRandomFileName(i)
+	);
 }
